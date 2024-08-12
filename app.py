03480f50ffd056113278e14b5b272c37bdb38fb4
kulaczanna <annakulacz@yahoo.com>
2024-08-12 11:40:22 +0200

    formatting

--- app.py
+++ app.py
@@ -1,162 +1,158 @@
-from flask import Flask, jsonify, request, Response
-import requests
 import csv
+from flask import Flask, jsonify, request, Response
 import io
 import json
+import requests
 
 app = Flask(__name__)
 
-API_URL = 'https://restcountries.com/v3.1'
+API_URL = "https://restcountries.com/v3.1"
+
 
 # Route to get the 10 biggest countries by area in a region
-@app.route('/api/region/<region>/biggest_countries_in_region', methods=['GET'])
+@app.route("/api/region/<region>/biggest_countries_in_region", methods=["GET"])
 def get_10_biggest_countries_by_area_for_region(region):
+
     # Construct the URL for fetching region data
-    request_url = f'{API_URL}/region/{region}'
-    
+    request_url = f"{API_URL}/region/{region}"
     # Send the request
     response = requests.get(request_url)
-    
+
     if response.status_code == 200:
         data = response.json()
 
         # Sort countries by area in descending order
-        sorted_countries = sorted(
-            data,
-            key=lambda country: country.get('area', 0),
-            reverse=True
+        sorted_data = sorted(
+            data, key=lambda country: country.get("area"), reverse=True
         )
 
-        # Get the top 10 largest countries
-        biggest_countries = sorted_countries[:10]
-        
+        # Get 10 biggest countries
+        biggest_countries = sorted_data[:10]
+
         # Build the result
         result = [
-                {
-                    'Country name': country.get('name', {}).get('common'),
-                    'Capital': country.get('capital')[0],
-                    'Region': country.get('region'),
-                    'Sub Region': country.get('subregion'),
-                    'Population': country.get('population'),
-                    'Area': country.get('area'),
-                    'Borders': country.get('borders')
-                }
-                for country in biggest_countries if country.get('area')
-            ]
-        
-        # Check if the user requested CSV format
-        if request.args.get('format') == 'csv':
-            # Create a CSV file in memory
-            output = io.StringIO()
-            writer = csv.DictWriter(output, fieldnames=["Country name", "Capital", "Region", "Sub Region", "Population", "Area", "Borders"])
-            writer.writeheader()
-            writer.writerows(result)
-            output.seek(0)
-            
-            # Return the CSV data as a response
-            return Response(output, mimetype="text/csv", headers={"Content-Disposition": "attachment;filename=biggest_countries_in_region.csv"})
-        else:
-            return Response(json.dumps(result, indent=4), mimetype='application/json')
-    else:
-        return jsonify({"error": "Failed to fetch data"}), response.status_code
-    
-# Route to get the countries with over 3 borders in a subregion
-@app.route('/api/subregion/<subregion>/countries_borders', methods=['GET'])
-def get_all_countries_with_over_3_borders_for_subregion(subregion):
-    # Construct the URL for fetching region data
-    request_url = f'{API_URL}/subregion/{subregion}'
-    
-    # Send the request
-    response = requests.get(request_url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        
-        result = [
-                {
-                    'Country name': country.get('name', {}).get('common'),
-                    'Capital': country.get('capital')[0],
-                    'Region': country.get('region'),
-                    'Sub Region': country.get('subregion'),
-                    'Population': country.get('population'),
-                    'Area': country.get('area'),
-                    'Borders': country.get('borders')
-                }
-                for country in data if len(country.get('borders')) > 3
+            {
+                "Country name": country.get("name").get("common"),
+                "Capital": country.get("capital")[0],
+                "Region": country.get("region"),
+                "Sub Region": country.get("subregion"),
+                "Population": country.get("population"),
+                "Area": country.get("area"),
+                "Borders": country.get("borders"),
+            }
+            for country in biggest_countries
+            if country.get("area")
         ]
 
         # Check if the user requested CSV format
-        if request.args.get('format') == 'csv':
-            # Create a CSV file in memory
+        if request.args.get("format") == "csv":
+            # Create a CSV file
             output = io.StringIO()
-            writer = csv.DictWriter(output, fieldnames=["Country name", "Capital", "Region", "Sub Region", "Population", "Area", "Borders"])
+            fieldnames = result[0].keys()
+            writer = csv.DictWriter(output, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(result)
             output.seek(0)
-            
+
             # Return the CSV data as a response
-            return Response(output, mimetype="text/csv", headers={"Content-Disposition": "attachment;filename=biggest_countries_in_region.csv"})
-        else:
-            return Response(json.dumps(result, indent=4), mimetype='application/json')
-        
+            return Response(output, mimetype="text/plain")
+
+        return Response(json.dumps(result, indent=4), mimetype="application/json")
     else:
         return jsonify({"error": "Failed to fetch data"}), response.status_code
-    
+
+
+# Route to get the countries with over 3 borders in a subregion
+@app.route("/api/subregion/<subregion>/countries_borders", methods=["GET"])
+def get_all_countries_with_over_3_borders_for_subregion(subregion):
+
+    request_url = f"{API_URL}/subregion/{subregion}"
+    response = requests.get(request_url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        result = [
+            {
+                "Country name": country.get("name").get("common"),
+                "Capital": country.get("capital")[0],
+                "Region": country.get("region"),
+                "Sub Region": country.get("subregion"),
+                "Population": country.get("population"),
+                "Area": country.get("area"),
+                "Borders": country.get("borders"),
+            }
+            for country in data
+            if len(country.get("borders")) > 3
+        ]
+
+        if request.args.get("format") == "csv":
+            output = io.StringIO()
+            fieldnames = result[0].keys()
+            writer = csv.DictWriter(output, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(result)
+            output.seek(0)
+
+            return Response(output, mimetype="text/plain")
+        else:
+            return Response(json.dumps(result, indent=4), mimetype="application/json")
+
+    else:
+        return jsonify({"error": "Failed to fetch data"}), response.status_code
+
+
 # Route to get the population in a subregion
-@app.route('/api/subregion/<subregion>/subregion_population', methods=['GET'])
+@app.route("/api/subregion/<subregion>/subregion_population", methods=["GET"])
 def get_the_population_for_subregion(subregion):
     # Construct the URL for fetching region data
-    request_url = f'{API_URL}/subregion/{subregion}'
-    
+    request_url = f"{API_URL}/subregion/{subregion}"
+
     # Send the request
     response = requests.get(request_url)
-    
+
     if response.status_code == 200:
         data = response.json()
 
         total_population = 0
 
-        # Calculate total population and list each country's population
+        # Calculate total population for subregion
         for country in data:
-            population = country.get('population')
+            population = country.get("population")
             total_population += population
-        
-        result = {
-            'subregion': country.get('subregion'),
-            'total_population': total_population,
-        }
 
-        
         result = [
-            
-                {
-                    'Country name': country.get('name', {}).get('common'),
-                    'Capital': country.get('capital')[0],
-                    'Region': country.get('region'),
-                    'Sub Region': country.get('subregion'),
-                    'Population': country.get('population'),
-                    'Area': country.get('area'),
-                    'Borders': country.get('borders'),
-                    f'Total population of {subregion.title()}': total_population
-                }
-                for country in data if country.get('population')
+            {
+                "Country name": country.get("name").get("common"),
+                "Capital": country.get("capital")[0],
+                "Region": country.get("region"),
+                "Sub Region": country.get("subregion"),
+                "Population": country.get("population"),
+                "Area": country.get("area"),
+                "Borders": country.get("borders"),
+                f"Total population of {subregion.title()}": total_population,
+            }
+            for country in data
+            if country.get("population")
         ]
 
-        # Check if the user requested CSV format
-        if request.args.get('format') == 'csv':
-            # Create a CSV file in memory
+        if request.args.get("format") == "csv":
             output = io.StringIO()
-            writer = csv.DictWriter(output, fieldnames=["Country name", "Capital", "Region", "Sub Region", "Population", "Area", "Borders", f"Total population of {subregion.title()}"])
+            fieldnames = result[0].keys()
+            writer = csv.DictWriter(output, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(result)
             output.seek(0)
-            
-            # Return the CSV data as a response
-            return Response(output, mimetype="text/csv", headers={"Content-Disposition": "attachment;filename=biggest_countries_in_region.csv"})
+
+            return Response(
+                output,
+                mimetype="text/plain",
+            )
         else:
-            return Response(json.dumps(result, indent=4), mimetype='application/json')
+            return Response(json.dumps(result, indent=4), mimetype="application/json")
     else:
         return jsonify({"error": "Failed to fetch data"}), response.status_code
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- unittests.py
+++ unittests.py
@@ -1,6 +1,7 @@
-import unittest
+from app import app
 import json
-from app import app 
+import unittest
+
 
 class APITestCase(unittest.TestCase):
     def setUp(self):
@@ -9,9 +10,9 @@ class APITestCase(unittest.TestCase):
         self.app.testing = True
 
     def test_get_10_biggest_countries_by_area_for_region_json(self):
-        # Send a GET request to the endpoint
-        response = self.app.get('/api/region/europe/biggest_countries_in_region')
 
+        # Send a GET request to the endpoint
+        response = self.app.get("/api/region/europe/biggest_countries_in_region")
         # Check that the status code is 200 (OK)
         self.assertEqual(response.status_code, 200)
 
@@ -24,153 +25,139 @@ class APITestCase(unittest.TestCase):
 
         # Check that each country has the expected keys
         for country in data:
-            self.assertIn('Country name', country)
-            self.assertIn('Capital', country)
-            self.assertIn('Region', country)
-            self.assertIn('Sub Region', country)
-            self.assertIn('Population', country)
-            self.assertIn('Area', country)
-            self.assertIn('Borders', country)
+            self.assertIn("Country name", country)
+            self.assertIn("Capital", country)
+            self.assertIn("Region", country)
+            self.assertIn("Sub Region", country)
+            self.assertIn("Population", country)
+            self.assertIn("Area", country)
+            self.assertIn("Borders", country)
 
     def test_get_10_biggest_countries_by_area_for_region_csv(self):
-        # Send a GET request to the endpoint with the CSV format
-        response = self.app.get('/api/region/europe/biggest_countries_in_region?format=csv')
 
+        # Send a GET request to the endpoint with the CSV format
+        response = self.app.get(
+            "/api/region/europe/biggest_countries_in_region?format=csv"
+        )
         # Check that the status code is 200 (OK)
         self.assertEqual(response.status_code, 200)
 
-        # Check that the response is of type CSV
-        self.assertEqual(response.mimetype, 'text/csv')
+        # Check that the response is of desired type
+        self.assertEqual(response.mimetype, "text/plain")
 
         # Check that the CSV contains the expected headers
-        csv_content = response.data.decode('utf-8')
-        self.assertIn('Country name', csv_content)
-        self.assertIn('Capital', csv_content)
-        self.assertIn('Region', csv_content)
-        self.assertIn('Sub Region', csv_content)
-        self.assertIn('Population', csv_content)
-        self.assertIn('Area', csv_content)
-        self.assertIn('Borders', csv_content)
+        country = response.data.decode("utf-8")
+        self.assertIn("Country name", country)
+        self.assertIn("Capital", country)
+        self.assertIn("Region", country)
+        self.assertIn("Sub Region", country)
+        self.assertIn("Population", country)
+        self.assertIn("Area", country)
+        self.assertIn("Borders", country)
 
     def test_region_not_found(self):
-        # Send a GET request to a non-existent region
-        # response = self.app.get('/api/region/Northern%20Europe/biggest_countries_in_region')
-        response = self.app.get('/api/region/nonexistentregion/biggest_countries_in_region')
 
-        # Check that the status code is not 200 (should be 404 or similar)
+        # Send a GET request to a non-existent region
+        response = self.app.get(
+            "/api/region/nonexistentregion/biggest_countries_in_region"
+        )
+
+        # Check that the status code is not 200
         self.assertNotEqual(response.status_code, 200)
 
         # Check that the response contains an error message
         data = json.loads(response.data)
-        self.assertIn('error', data)
+        self.assertIn("error", data)
 
     def test_get_all_countries_with_over_3_borders_for_subregion_json(self):
-            # Send a GET request to the endpoint
-            response = self.app.get('/api/subregion/central%20europe/countries_borders')
 
-            # Check that the status code is 200 (OK)
-            self.assertEqual(response.status_code, 200)
+        response = self.app.get("/api/subregion/central%20europe/countries_borders")
+        self.assertEqual(response.status_code, 200)
 
-            # Check that the response is valid JSON
-            data = json.loads(response.data)
-            self.assertIsInstance(data, list)
+        data = json.loads(response.data)
+        self.assertIsInstance(data, list)
 
-            # Check that each country has the expected keys
-            for country in data:
-                self.assertIn('Country name', country)
-                self.assertIn('Capital', country)
-                self.assertIn('Region', country)
-                self.assertIn('Sub Region', country)
-                self.assertIn('Population', country)
-                self.assertIn('Area', country)
-                self.assertIn('Borders', country)
+        for country in data:
+            self.assertIn("Country name", country)
+            self.assertIn("Capital", country)
+            self.assertIn("Region", country)
+            self.assertIn("Sub Region", country)
+            self.assertIn("Population", country)
+            self.assertIn("Area", country)
+            self.assertIn("Borders", country)
 
     def test_get_all_countries_with_over_3_borders_for_subregion_csv(self):
-        # Send a GET request to the endpoint with the CSV format
-        response = self.app.get('/api/subregion/central%20europe/countries_borders?format=csv')
 
-        # Check that the status code is 200 (OK)
+        response = self.app.get(
+            "/api/subregion/central%20europe/countries_borders?format=csv"
+        )
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "text/plain")
 
-        # Check that the response is of type CSV
-        self.assertEqual(response.mimetype, 'text/csv')
+        country = response.data.decode("utf-8")
+        self.assertIn("Country name", country)
+        self.assertIn("Capital", country)
+        self.assertIn("Region", country)
+        self.assertIn("Sub Region", country)
+        self.assertIn("Population", country)
+        self.assertIn("Area", country)
+        self.assertIn("Borders", country)
 
-        # Check that the CSV contains the expected headers
-        csv_content = response.data.decode('utf-8')
-        self.assertIn('Country name', csv_content)
-        self.assertIn('Capital', csv_content)
-        self.assertIn('Region', csv_content)
-        self.assertIn('Sub Region', csv_content)
-        self.assertIn('Population', csv_content)
-        self.assertIn('Area', csv_content)
-        self.assertIn('Borders', csv_content)
+    def test_subregion_not_found_borders(self):
 
-    def test_subregion_not_found(self):
-        # Send a GET request to a non-existent region
-        # response = self.app.get('/api/region/Northern%20Europe/biggest_countries_in_region')
-        response = self.app.get('/api/region/nonexistentregion/biggest_countries_in_region')
-
-        # Check that the status code is not 200 (should be 404 or similar)
+        response = self.app.get("/api/subregion/nonexistentsubregion/countries_borders")
         self.assertNotEqual(response.status_code, 200)
 
-        # Check that the response contains an error message
         data = json.loads(response.data)
-        self.assertIn('error', data)
+        self.assertIn("error", data)
 
     def test_get_the_population_for_subregion_json(self):
-            # Send a GET request to the endpoint
-            response = self.app.get('/api/subregion/central%20europe/region_population')
 
-            # Check that the status code is 200 (OK)
-            self.assertEqual(response.status_code, 200)
-
-            # Check that the response is valid JSON
-            data = json.loads(response.data)
-            self.assertIsInstance(data, list)
-
-            # Check that each country has the expected keys
-            for country in data:
-                self.assertIn('Country name', country)
-                self.assertIn('Capital', country)
-                self.assertIn('Region', country)
-                self.assertIn('Sub Region', country)
-                self.assertIn('Population', country)
-                self.assertIn('Area', country)
-                self.assertIn('Borders', country)
-                self.assertIn(f'Total population of Central Europe', country)
-
-    def test_get_the_population_for_subregion_csv(self):
-        # Send a GET request to the endpoint with the CSV format
-        response = self.app.get('/api/subregion/central%20europe/region_population?format=csv')
-
-        # Check that the status code is 200 (OK)
+        response = self.app.get("/api/subregion/central%20europe/subregion_population")
         self.assertEqual(response.status_code, 200)
 
-        # Check that the response is of type CSV
-        self.assertEqual(response.mimetype, 'text/csv')
+        data = json.loads(response.data)
+        self.assertIsInstance(data, list)
 
-        # Check that the CSV contains the expected headers
-        csv_content = response.data.decode('utf-8')
-        self.assertIn('Country name', csv_content)
-        self.assertIn('Capital', csv_content)
-        self.assertIn('Region', csv_content)
-        self.assertIn('Sub Region', csv_content)
-        self.assertIn('Population', csv_content)
-        self.assertIn('Area', csv_content)
-        self.assertIn('Borders', csv_content)
-        self.assertIn('Total population of Central Europe', csv_content)
-        
-    def test_subregion_not_found(self):
-        # Send a GET request to a non-existent region
-        # response = self.app.get('/api/region/Northern%20Europe/biggest_countries_in_region')
-        response = self.app.get('/api/region/nonexistentregion/biggest_countries_in_region')
+        for country in data:
+            self.assertIn("Country name", country)
+            self.assertIn("Capital", country)
+            self.assertIn("Region", country)
+            self.assertIn("Sub Region", country)
+            self.assertIn("Population", country)
+            self.assertIn("Area", country)
+            self.assertIn("Borders", country)
+            self.assertIn(f"Total population of Central Europe", country)
 
-        # Check that the status code is not 200 (should be 404 or similar)
+    def test_get_the_population_for_subregion_csv(self):
+
+        response = self.app.get(
+            "/api/subregion/central%20europe/subregion_population?format=csv"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "text/plain")
+
+        country = response.data.decode("utf-8")
+        self.assertIn("Country name", country)
+        self.assertIn("Capital", country)
+        self.assertIn("Region", country)
+        self.assertIn("Sub Region", country)
+        self.assertIn("Population", country)
+        self.assertIn("Area", country)
+        self.assertIn("Borders", country)
+        self.assertIn("Total population of Central Europe", country)
+
+    def test_subregion_not_found_total_population(self):
+
+        response = self.app.get(
+            "/api/subregion/nonexistentsubregion/subregion_population"
+        )
         self.assertNotEqual(response.status_code, 200)
 
-        # Check that the response contains an error message
         data = json.loads(response.data)
-        self.assertIn('error', data)
+        self.assertIn("error", data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
